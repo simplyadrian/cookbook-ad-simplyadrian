c95ab90a82050a4762fefb272f41dca9560d6831
Jesse Hauf <jesse.hauf@nativex.com>
2015-03-25 09:52:54 -0500

    Refactor and lowered debug level

--- attributes/sssd_ldap.rb
+++ attributes/sssd_ldap.rb
@@ -1,6 +1,6 @@
 default['ad-nativex']['sssd_ldap']['id_provider'] = 'ad'
-default['ad-nativex']['sssd_ldap']['auth_provider'] = 'krb5'
-default['ad-nativex']['sssd_ldap']['chpass_provider'] = 'krb5'
+default['ad-nativex']['sssd_ldap']['auth_provider'] = 'ldap'
+default['ad-nativex']['sssd_ldap']['chpass_provider'] = 'ldap'
 default['ad-nativex']['sssd_ldap']['sudo_provider'] = 'ldap'
 default['ad-nativex']['sssd_ldap']['enumerate'] = 'true'
 default['ad-nativex']['sssd_ldap']['cache_credentials'] = 'true'
@@ -20,16 +20,20 @@ default['ad-nativex']['sssd_ldap']['ldap_group_object_class'] = 'group'
 
 default['ad-nativex']['sssd_ldap']['ldap_id_use_start_tls'] = 'false'
 default['ad-nativex']['sssd_ldap']['ldap_tls_reqcert'] = 'never'
-default['ad-nativex']['sssd_ldap']['ldap_tls_cacert'] = value_for_platform_family('rhel' => '/etc/pki/tls/certs/ca-bundle.crt', 'default' => '/etc/ssl/certs/ca-certificates.crt')
-
+default['ad-nativex']['sssd_ldap']['ldap_tls_cacert'] = value_for_platform_family(
+                                                          'rhel' => '/etc/pki/tls/certs/ca-bundle.crt',
+                                                          'default' => '/etc/ssl/certs/ca-certificates.crt'
+                                                        )
 # If you have a domain that doesn't require binding set these two attributes to nil
 default['ad-nativex']['sssd_ldap']['ldap_default_bind_dn'] = nil
 default['ad-nativex']['sssd_ldap']['ldap_default_authtok'] = nil
 
-default['ad-nativex']['sssd_ldap']['authconfig_params'] = '--enablesssd --enablesssdauth --enablelocauthorize --enablemkhomedir --update'
-default['ad-nativex']['sssd_ldap']['access_provider'] = 'ad'
+default['ad-nativex']['sssd_ldap']['authconfig_params'] = '--enablesssd --enablesssdauth --enablelocauthorize '\
+                                                            '--enablemkhomedir --update'
 
-# Can use simple LDAP filter such as 'uid=abc123' or more expressive LDAP filters like '(&(objectClass=employee)(department=ITSupport))'
+# Can use simple LDAP filter such as 'uid=abc123' or more expressive LDAP filters
+# like '(&(objectClass=employee)(department=ITSupport))'
+default['ad-nativex']['sssd_ldap']['access_provider'] = nil # 'ad', 'ldap', or 'krb5'
 default['ad-nativex']['sssd_ldap']['ldap_access_filter'] = nil
 
 default['ad-nativex']['sssd_ldap']['min_id'] = '1'
@@ -43,4 +47,4 @@ default['ad-nativex']['sssd_ldap']['dyndns_update_ptr'] = true
 default['ad-nativex']['sssd_ldap']['dyndns_ttl'] = 3600
 
 # Debug
-default['ad-nativex']['sssd_ldap']['debug_level'] = 9 # TODO: Change to 0
+default['ad-nativex']['sssd_ldap']['debug_level'] = 5 # TODO: Change to 0
